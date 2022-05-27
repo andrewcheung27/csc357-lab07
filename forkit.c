d4b379777eec5304dd1f49ca3523a51193a38ac0
andrewcheung27 <andrewcheung27@gmail.com>
2022-05-26 18:49:57 -0700

    forked it

--- forkit.c
+++ forkit.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 
 int main(int argc, char *argv[]) {
-    return 0;
+    int status;
+
+    printf("Hello, world!\n");
+
+    if (fork()) {
+        printf("This is the parent, pid %d.\n", getpid());
+    }
+    else {
+        printf("This is the child, pid %d.\n", getpid());
+    }
+
+    wait(&status);
+    printf("This is the parent, pid %d, signing off.\n", getpid());
 }
 
